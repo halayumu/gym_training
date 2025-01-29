14a50558af9fd793ec18ed5d0fedeecfb9c479b3
halayumu <ookamiayumuwww@gmail.com>
2025-01-29 12:26:45 +0900

    トレーニングメニューを入力

---
+++ example-app/resources/views/TrainingDetail.blade.php
@@ -0,0 +1,14 @@
+<div>
+    <!-- Live as if you were to die tomorrow. Learn as if you were to live forever. - Mahatma Gandhi -->
+
+    <div class="box">
+<div class="list">
+    <div class="weight">
+<input type="text" name="weight">
+    </div>
+<div class="rep">
+<input type="text" name="rep">
+</div>
+</div>
+    </div>
+</div>
