f7b2ad853fa75c11152911d66440dba16f1a5992
halayumu <ookamiayumuwww@gmail.com>
2025-01-29 12:28:47 +0900

    トレーニング内容確認画面

---
+++ example-app/resources/views/TrainingDetailIndex.blade.php
@@ -0,0 +1,19 @@
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Live as if you were to die tomorrow. Learn as if you were to live forever. - Mahatma Gandhi -->
+
+    <div class="menu-box">
+        <div class="list">
+            <div class="weight-title">
+                重量
+            </div>
+            <div class="weight">
+                {{ $weight }}kg
+            </div>
+            <div class="rep-title">
+                回数
+            </div>
+            <div class="rep">
+                {{ $rep }}回数
+            </div>
+        </div>
+    </div>
