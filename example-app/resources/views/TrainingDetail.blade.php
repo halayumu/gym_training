83782347888dfc5a3d60b1eea2447ae03fd917f5
halayumu <ookamiayumuwww@gmail.com>
2025-02-01 14:16:21 +0900

    戻るボタンがタップされた時に入力値を保持

--- example-app/resources/views/TrainingDetail.blade.php
+++ example-app/resources/views/TrainingDetail.blade.php
@@ -1,14 +1,17 @@
-<div>
-    <!-- Live as if you were to die tomorrow. Learn as if you were to live forever. - Mahatma Gandhi -->
+<form action="/TrainingDetail" method="post">
+    @csrf
+    <div>
+        <!-- Live as if you were to die tomorrow. Learn as if you were to live forever. - Mahatma Gandhi -->
+        <div class="box">
+    <div class="list">
+        <div class="weight">
+            <span>重量</span>
+    <input type="text" name="weight" value="{{ session('weight', '') }}">
+        </div>
+    <div class="rep">
+    <span>回数</span>
+    <input type="text" name="rep" value="{{ session('rep', '') }}">
 
-    <div class="box">
-<div class="list">
-    <div class="weight">
-<input type="text" name="weight">
+    <button type="submit">送信</button>
     </div>
-<div class="rep">
-<input type="text" name="rep">
-</div>
-</div>
-    </div>
-</div>
+</form>
