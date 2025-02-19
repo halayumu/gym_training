2c55ea67c14501958ccc0b27345a772dda6658a8
halayumu <ookamiayumuwww@gmail.com>
2025-02-19 18:00:08 +0900

    トレーニングメニューの追加処理(仮データの登録のみ一旦)

--- example-app/app/Models/TrainingDetail.php
+++ example-app/app/Models/TrainingDetail.php
@@ -3,8 +3,46 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class TrainingDetail extends Model
 {
-    //
+
+    /**
+     * トレーニングメニューの追加
+     * 
+     * ①ログインしているユーザidを取得
+     * ②メニューテーブルに①のユーザーidを外部キーとして追加
+     * 
+     * @param $weight 重量
+     * @param $rep 回数
+     */
+    public function addTrainingMenu($weight, $rep)
+    {
+        $userId = 2; //ログインしているユーザのユーザidを習得
+
+        // 仮データ
+        $part = '胸';
+        $trainingExerciseName = 'ベンチプレス';
+        $set = 3;
+        $rep = 10;
+        $weight = 100;
+        $supportHeight = 100;
+        $chairHeight = 4;
+        $supportHeight = 5;
+        $memo = 'メモ';
+
+        // menuテーブルに値を追加する
+        DB::table('menu')->insert([
+            'part' => $part,
+            'trainingExerciseName' => $trainingExerciseName,
+            'set' => $set,
+            'rep' => $rep,
+            'weight' => $weight,
+            'chairHeight' => $chairHeight,
+            'supportHeight' => $supportHeight,
+            'memo' => $memo,
+            'user_id' => $userId,
+        ]);
+    }
 }
