f37f9d61da4c6cd1afac8f4b260c740a52e307d9
halayumu <ookamiayumuwww@gmail.com>
2025-02-20 19:57:28 +0900

    ホーム画面のhtml部分の作成(途中)

--- example-app/resources/views/Home.blade.php
+++ example-app/resources/views/Home.blade.php
@@ -1,3 +1,21 @@
-<div>
-    <!-- People find pleasure in different ways. I find it in keeping my mind clear. - Marcus Aurelius -->
+<div class="menu_box">
+    <div class="label">
+        <input type="radio" class="" id="all" checked>
+        <label for="all">All</label>
+
+        <input type="radio" class="" id="chest" checked>
+        <label for="chest">胸</label>
+
+        <input type="radio" class="" id="shoulder" checked>
+        <label for="shoulder">肩</label>
+
+        <input type="radio" class="" id="arm" checked>
+        <label for="arm">腕</label>
+
+        <input type="radio" class="" id="back" checked>
+        <label for="back">背中</label>
+
+        <input type="radio" class="" id="leg" checked>
+        <label for="leg">脚</label>
+    </div>
 </div>
