a125a862ef689aed64f972bd17b988b7f9099984
halayumu <ookamiayumuwww@gmail.com>
2025-03-04 11:26:16 +0900

    ホーム画面の追加とトレメニューの検索をするAPIからの処理を追加

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -10,4 +10,5 @@ Route::get('/', function () {
 Route::get('/TrainingInput', [TrainingDetailController::class, 'index']); //入力画面
 Route::post('/TrainingDetail', [TrainingDetailController::class, 'index']); //詳細画面
 Route::post('/TrainingMenuAdd', [TrainingDetailController::class, 'create']); //トレーニングメニュー登録
-Route::get('/Home', [TrainingDetailController::class, 'Home']);//ホーム画面
+Route::get('/Home', [TrainingDetailController::class, 'Home']); //ホーム画面
+Route::post('/Home/search', [TrainingDetailController::class, 'searchMenu']);//メニュー検索
