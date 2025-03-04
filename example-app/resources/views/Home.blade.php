0db91b704fdac743a6ed7128246f3335f0950f1a
halayumu <ookamiayumuwww@gmail.com>
2025-03-04 11:26:56 +0900

    ホーム画面の作成。メタタグの追加

--- example-app/resources/views/Home.blade.php
+++ example-app/resources/views/Home.blade.php
@@ -1,21 +1,29 @@
-<div class="menu_box">
-    <div class="label">
-        <input type="radio" class="" id="all" checked>
-        <label for="all">All</label>
+<head>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+</head>
 
-        <input type="radio" class="" id="chest" checked>
-        <label for="chest">胸</label>
-
-        <input type="radio" class="" id="shoulder" checked>
-        <label for="shoulder">肩</label>
-
-        <input type="radio" class="" id="arm" checked>
-        <label for="arm">腕</label>
-
-        <input type="radio" class="" id="back" checked>
-        <label for="back">背中</label>
-
-        <input type="radio" class="" id="leg" checked>
-        <label for="leg">脚</label>
+<body>
+    <div class="menu_box">
+        <div class="label">
+            <input type="radio" class="" id="all" checked>
+            <label for="all">All</label>
+    
+            <input type="radio" class="" id="chest" checked>
+            <label for="chest">胸</label>
+    
+            <input type="radio" class="" id="shoulder" checked>
+            <label for="shoulder">肩</label>
+    
+            <input type="radio" class="" id="arm" checked>
+            <label for="arm">腕</label>
+    
+            <input type="radio" class="" id="back" checked>
+            <label for="back">背中</label>
+    
+            <input type="radio" class="" id="leg" checked>
+            <label for="leg">脚</label>
+        </div>
     </div>
-</div>
+
+    @vite('resources/js/home.js')
+</body>
