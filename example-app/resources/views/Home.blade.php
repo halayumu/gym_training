f3d89ae0423c4413a872b89797c2e3a8341be490
halayumu <ookamiayumuwww@gmail.com>
2025-03-17 19:02:58 +0900

    ホーム画面：トレーニング追加画面に遷移するためのボタン作成

--- example-app/resources/views/Home.blade.php
+++ example-app/resources/views/Home.blade.php
@@ -1,34 +1,40 @@
+@vite('resources/css/app.css')
+
 <head>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 
 <body>
     <div class="menu_box">
+
         <div class="label">
-            <input type="radio" class="" id="all" checked>
-            <label for="all">All</label>
+            <input type="radio" class="tab_button" id="all" checked>
+            <label for="all" class="tab_label_right">All</label>
     
-            <input type="radio" class="" id="chest" checked>
-            <label for="chest">胸</label>
+            <input type="radio" class="tab_button" id="chest" checked>
+            <label for="chest" class="tab_label_right">胸</label>
     
-            <input type="radio" class="" id="shoulder" checked>
-            <label for="shoulder">肩</label>
+            <input type="radio" class="tab_button   " id="shoulder" checked>
+            <label for="shoulder" class="tab_label_right">肩</label>
     
-            <input type="radio" class="" id="arm" checked>
-            <label for="arm">腕</label>
+            <input type="radio" class="tab_button" id="arm" checked>
+            <label for="arm" class="tab_label_right">腕</label>
     
-            <input type="radio" class="" id="back" checked>
-            <label for="back">背中</label>
+            <input type="radio" class="tab_button" id="back" checked>
+            <label for="back" class="tab_label_right">背中</label>
     
-            <input type="radio" class="" id="leg" checked>
+            <input type="radio" class="tab_button" id="leg" checked>
             <label for="leg">脚</label>
         </div>
 
         <div class="list_box">
             <ul id="tore_menu">
             </ul>
-            <button id="viewToggleButton">全て表示</button>
         </div>
+    </div>
+
+    <div class="circle_button">
+        <span>+</span>
     </div>
 
     @vite('resources/js/home.js')
