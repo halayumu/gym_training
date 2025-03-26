42878a2b54b3058ceba0b267e351ebbc89fd8b8e
halayumu <ookamiayumuwww@gmail.com>
2025-03-26 23:55:19 +0900

    トレーニング登録画面：トレーニングのプルダウンをjs側で取得するためにidの追加

--- example-app/resources/views/AddTraining.blade.php
+++ example-app/resources/views/AddTraining.blade.php
@@ -1,23 +1,30 @@
-<form action="/AddTraining" method="post">
+<head>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+</head>
+
+{{-- <form action="/AddTraining" method="post"> --}}
     @csrf
 <div>
     <h2>部位</h2>
-    <select name="part">
+    <select name="part" id="part">
         @foreach ($menuPart as $part)
         <option value="{{ $part }}">{{ $part }}</option>
         @endforeach
     </select>
 
     <h2>種目</h2>
-    <input type="text" name="trainingExerciseName">
+    <input type="text" name="trainingExerciseName" id="trainingExerciseName">
 
     <h2>曜日</h2>
-    <select name="weekday">
+    <select name="weekday" id="weekday">
         @foreach ($menuDay as $day)
         <option value="{{ $day }}">{{ $day }}</option>
         @endforeach
     </select>
 
-    <button>登録</button>
+    <button id="add_button">登録</button>
+
 </div>
-</form>
+{{-- </form> --}}
+
+@vite('resources/js/AddTraining.js')
