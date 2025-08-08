604b8c8393711ddc6345cb10d58cf5301e26c030
halayumu <ookamiayumuwww@gmail.com>
2025-08-08 17:25:55 +0900

    top画面：デザインに伴うクラスを追加

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,16 +1,18 @@
 @vite(['resources/css/top.css'])
 
-<div>
+<div class="top-box">
+    <div class="top-icon">
+        <img src="{{ asset('images/log.png') }}" alt="WORKOUT">
+    </div>
 
-    <img src="{{ asset('images/log.png') }}" alt="WORKOUT">
+    <div class="top-title">
+        <h1>WORKOUT</h1>
+    </div>
 
-    <h1>WORKOUT</h1>
-
-<div class="btn-size">
-    <a href="{{ route('login') }}">ログイン</a>
-</div>
-<div class="btn-size">
-    <a href="">新規登録</a>
-</div>
-    
+    <div class="btn-size">
+        <a href="{{ route('login') }}">ログイン</a>
+    </div>
+    <div class="btn-size">
+        <a href="">新規登録</a>
+    </div>
 </div>
